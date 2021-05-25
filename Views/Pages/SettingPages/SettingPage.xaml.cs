001d3801a1ab964212c871dc7362e1a91afc9d1e
Domovski1 <624273242@mail.ru>
2021-05-25 20:32:49 +0300

    Инициализорвана странциа настроек

---
+++ Views/Pages/SettingPages/SettingPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tesis.Properties;
+
+namespace Tesis.Views.Pages.SettingPages
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingPage.xaml
+    /// </summary>
+    public partial class SettingPage : Page
+    {
+        public SettingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_btn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Path_Txb.Text = Settings.Default.PathToDocs;
+        }
+    }
+}
