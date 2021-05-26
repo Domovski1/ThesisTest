90d3aade1e699f50765e9e6c9027798a06061d02
Domovski1 <624273242@mail.ru>
2021-05-26 08:44:08 +0300

    Добавлен обрабочтик ошибок на страницу с добавлением пропуска

--- Views/Pages/AttendancePage.xaml.cs
+++ Views/Pages/AttendancePage.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Tesis.Model;
 
 namespace Tesis.Views.Pages
@@ -52,7 +43,16 @@ namespace Tesis.Views.Pages
 
         private void Save_Btn_Click(object sender, RoutedEventArgs e)
         {
-            //AppData.db.SaveChanges();
+            try
+            {
+                AppData.db.SaveChanges();
+                MessageBox.Show("Сохранено!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
